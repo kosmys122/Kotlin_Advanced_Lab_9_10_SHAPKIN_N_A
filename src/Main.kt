424703efdce278ce23432b42774e7f21696148bd
Шапкин Николай <kosmys122@gmail.com>
2026-01-11 20:53:50 +0300

    Create sealed model ModuleResult

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,6 @@
+import example.age
 import modules.EnergyGenerator
+import modules.ModuleResult
 import modules.ResearchLab
 import resources.OutpostResource
 import resources.ResourceManager
@@ -12,13 +14,28 @@ fun main(){
 //    manager.printAll()
 //    val bonus=minerals.copy(amount = minerals.amount+50)
 //    println("Копия минералов с бонусом: $bonus")
-    val manager= ResourceManager()
-    manager.add(OutpostResource(1,"Minerals",120))
-    manager.add(OutpostResource(2,"Gas",40))
-    val generator= EnergyGenerator()
-    val lab= ResearchLab()
-    generator.performAction(manager)
-    lab.performAction(manager)
+//    val manager= ResourceManager()
+//    manager.add(OutpostResource(1,"Minerals",120))
+//    manager.add(OutpostResource(2,"Gas",40))
+//    val generator= EnergyGenerator()
+//    val lab= ResearchLab()
+//    generator.performAction(manager)
+//    lab.performAction(manager)
+//    println()
+//    manager.printAll()
+    val generatorResult=generator.performAction(manager)
+    val labResult=lab.performAction(manager)
+    handleModuleResult(generatorResult)
+    handleModuleResult(labResult)
     println()
     manager.printAll()
+}
+fun handleModuleResult(result: ModuleResult){
+    when (result){
+        is ModuleResult.Success-> println("УСПЕХ:${result.message}")
+        is ModuleResult.ResourceProduced-> println("Произведено: ${result.resourceName} +${result.amount}")
+        is ModuleResult.NotEnoughResources-> println("Недостаточно ресурса ${result.resourceName}." +
+                "Нужно: ${result.required}, есть: ${result.available}")
+        is ModuleResult.Error-> println("ОШИБКА: ${result.reason}")
+    }
 }
--- src/payment/Main.kt
+++ src/payment/Main.kt
@@ -1,18 +1,19 @@
 package payment
 
-fun main(){
-    val processor=PaymentProcessor()
-    val payments= listOf(
-        Payment("4_111_111_111_111_111",1000,CardType.VISA),
-        Payment("5_111_111_111_111_111",2000,CardType.MASTERCARD),
-        Payment(2_222_222_222_222_222,1500,CardType.MIR),
-        Payment("1234567812345678","500",CardType.UNKNOWN),
-        Payment("123",3000,CardType.VISA)
+fun main() {
+    val processor = PaymentProcessor()
+    val payments = listOf(
+        Payment("4_111_111_111_111_111", 1000, CardType.VISA),
+        Payment("5_111_111_111_111_111", 2000, CardType.MASTERCARD),
+        Payment("2_222_222_222_222_222", 1500, CardType.MIR),
+        Payment("1234567812345678", 500, CardType.UNKNOWN),
+        Payment("123", 3000, CardType.VISA)
     )
     println("=== Обработка платежей ===")
-    payments.forEach { payment-> println("\nПлатёж ${payment.type}: ${payment.card.take(4)}..., ${payment.sum} руб") }
-    val result=processor.pay(payment)
+    payments.forEach { payment -> println("\nПлатёж ${payment.type}: ${payment.card.take(4)}..., ${payment.sum} руб")
+    val result = processor.pay(payment)
     processor.show(result)
+    }
     println("\n=== Работа с enum ===")
     val cardType=CardType.VISA
     println("Тип карты: $cardType")
