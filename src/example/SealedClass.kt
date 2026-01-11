d7bee744cb9c161952a634696142b7b610fc8297
Шапкин Николай <kosmys122@gmail.com>
2026-01-11 17:38:16 +0300

    learn sealed-class

--- src/example/SealedClass.kt
+++ src/example/SealedClass.kt
@@ -21,10 +21,28 @@ fun handleResult(result: NetworkResult){
 }
 
 fun main(){
-    val success=NetworkResult.Success("Данные получены")
-    val error=NetworkResult.Error("Сервер не отвечает",500)
-    val loading=NetworkResult.Loading
-    handleResult(success)
-    handleResult(error)
-    handleResult(loading)
+//    val success=NetworkResult.Success("Данные получены")
+//    val error=NetworkResult.Error("Сервер не отвечает",500)
+//    val loading=NetworkResult.Loading
+//    handleResult(success)
+//    handleResult(error)
+//    handleResult(loading)
+    handleOrder(OrderStatys.Created)
+    handleOrder(OrderStatys.Paid)
+    handleOrder(OrderStatys.Shipped)
+    handleOrder(OrderStatys.Cancelled("Нет товара на складе"))
+}
+sealed class OrderStatys{
+    object Created:OrderStatys()
+    object Paid:OrderStatys()
+    object Shipped:OrderStatys()
+    data class Cancelled(val reason:String):OrderStatys()
+}
+fun handleOrder(statys: OrderStatys){
+    when (statys){
+        OrderStatys.Created-> println("Заказ создан")
+        OrderStatys.Paid-> println("Заказ оплачен")
+        OrderStatys.Shipped-> println("Заказ отправлен")
+        is OrderStatys.Cancelled-> println("Отменён: ${statys.reason}")
+    }
 }
